4185afd20d7d31b36d0bd90219f34d9cfbfdd5ae
Ben McCallum <ben.mccallum@isobar.com>
2014-09-25 11:54:42 +1000

    Initial commit
    Probably needs tidying up.

---
+++ CoreMvc/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace CoreMvc.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string GetCanonicalUrl(string path)
+        {
+            path = path.TrimEnd('/').ToLower();
+            const string indexDefault = "/index";
+            if (path.EndsWith(indexDefault))
+            {
+                path = path.Substring(0, path.Length - indexDefault.Length);
+            }
+            return path;
+        }
+
+        public static string GetCanonicalUrl(this UrlHelper url, string path = null)
+        {
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                var rawUrl = url.RequestContext.HttpContext.Request.Url;
+                if (rawUrl == null)
+                {
+                    return null;
+                }
+                path = String.Format("{0}://{1}{2}", rawUrl.Scheme, rawUrl.Host, rawUrl.AbsolutePath);
+            }
+
+            return GetCanonicalUrl(path);
+        }
+
+        public static MvcHtmlString CanonicalUrlLink(this HtmlHelper html, string path = null)
+        {
+            var canonical = new TagBuilder("link");
+            canonical.MergeAttribute("rel", "canonical");
+            canonical.MergeAttribute("href", GetCanonicalUrl(path));
+            return new MvcHtmlString(canonical.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+CoreMvc
+==========
+
+A library of core helpers, extensions, constants, enums and other useful things for all ASP.NET MVC projects.
