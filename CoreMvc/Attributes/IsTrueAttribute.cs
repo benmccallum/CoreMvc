68bb36457ed79e6dac8317d076ffed131dfe7f1d
Ben McCallum <ben.mccallum@isobar.com>
2014-10-03 17:58:50 +1000

    Adding the IsTrue validation attribute for a boolean property.

---
+++ CoreMvc/Attributes/IsTrueAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreMvc.Attributes
+{
+    /// <summary>
+    /// Attribute for validating that a boolean property has a value of TRUE.
+    /// </summary>
+    public class IsTrueAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            if (value.GetType() != typeof(bool))
+            {
+                throw new InvalidOperationException("Can only be used on boolean properties.");
+            }
+
+            return (bool)value;
+        }
+    }
+}
